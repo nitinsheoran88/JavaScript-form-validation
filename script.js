2ad2ec76b06d9b756b77fe9c92b0e7183886125a
nitinsheoran88 <nitinsheoran47@gmail.com>
2025-03-11 21:45:47 +0530

    project-updated

--- script.js
+++ script.js
@@ -5,24 +5,28 @@ document.querySelector('#form-submit').addEventListener('click', function(){
 
     if(document.querySelector('.firstname').value === ''){
         document.querySelector('.error').innerText = 'please complete the details .'
+        return;
     }else{
         document.querySelector('.error').innerText = ''
     }
     
     if(document.querySelector('.lastname').value === ''){
         document.querySelector('.error').innerText = 'please complete the details of last name column .'
+        return;
     }else{
         document.querySelector('.error').innerText = ''
     }
 
     if(document.querySelector('.address').value === ''){
         document.querySelector('.error').innerText = 'please complete the details of address column.'
+        return;
     }else{
         document.querySelector('.error').innerText = ''
     }
     
     if(document.querySelector('.phonenumber').value === ''){
         document.querySelector('.error').innerText = 'please complete the details of phone number column.'
+        return;
     }else{
         document.querySelector('.error').innerText = ''
     }
